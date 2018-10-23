090996b598d6f1c17e9c271661c7cf144ab156e1
cvillarreal1997 <terrycarlos@live.com>
2018-10-23 14:49:40 -0500

    labs 7

---
+++ workshops/partial1/Labs7/Animal.java
@@ -0,0 +1,41 @@
+public class Animal{
+  private int id;
+  private String name;
+  private String type;
+  private boolean food;
+    public void Feed(){
+		food=true;
+		
+	}
+	 public void print(){
+		 System.out.println("The Animal name is "+name+",is a:"+type+",+ ate" +food);	 
+		
+		
+	}
+	
+	
+	
+	public Animal(){
+		id=1;
+		name="Snappy";
+		type="Dog";
+		food=true;
+		
+	}
+	
+	public void unfeed(){
+		
+		
+		
+	}
+	public void Animal(int id,String name,String type,boolean food){	
+		
+		
+	}
+	
+      
+ }
+ 
+  
+  
+ 
