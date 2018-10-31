5b4a8a7435be97e0ba6e5e13240a319b42e7bf45
cvillarreal1997 <terrycarlos@live.com>
2018-10-30 21:18:31 -0500

    Labs 8

---
+++ workshops/partial1/Labs8/ZooProject/src/ec/edu/espe/www/zooproject/model/Worker.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.www.zooproject.model;
+
+/**
+ *
+ * @author USER
+ */
+public class Worker {
+    private String name;
+
+    public Worker() {
+    }
+
+    public Worker(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   
+
+    
+    
+    
+}
